c8cb7902a3b50e1688620979ee8aef66e60aecf8
MuskanNadeem55 <160000879+MuskanNadeem55@users.noreply.github.com>
2024-04-17 07:30:32 -0700

    Changes updated

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+import chalk from "chalk";
+
+console.log(chalk.bgGray.bold("\t\t ***WELCOME TO THE WORD COUNTER*** \t\t"));
+
+const answer:{
+    sentence:string
+} = await inquirer.prompt([
+    {
+        name: "sentence",
+        type:"input",
+        message: (chalk.bgBlueBright.italic("\n Enter your sentence to count the word "))
+    }
+]);
+
+let words =answer.sentence.trim().split(" ");
+
+console.log(words);
+
+console.log(
+    chalk.bgBlueBright.italic.bold(`\n Your sentence word count is ${words.length}`)
+);
+
+
